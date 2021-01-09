36e508ad592508c0992cfc41890d59b811562b80
Eric Baas <ebaas@extron.com>
2021-01-09 09:36:37 -0800

    basic file upload and ui working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,57 @@
-import { Component } from '@angular/core';
-import { ElectronService } from './core/services';
-import { TranslateService } from '@ngx-translate/core';
-import { AppConfig } from '../environments/environment';
-
+import { Component, ViewChild, OnInit, AfterViewInit } from "@angular/core";
+import { MatPaginator } from "@angular/material/paginator";
+import { MatSort } from "@angular/material/sort";
+import { MatTableDataSource } from "@angular/material/table";
+import { FileUploader } from "ng2-file-upload";
+const fs: any = window.require("fs");
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
-export class AppComponent {
-  constructor(
-    private electronService: ElectronService,
-    private translate: TranslateService
-  ) {
-    this.translate.setDefaultLang('en');
-    console.log('AppConfig', AppConfig);
+export class AppComponent implements OnInit, AfterViewInit {
+  dataSource = new MatTableDataSource<any>();
+  @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+  @ViewChild("fileInput") fileInput;
+  private data = [];
+  public file;
 
-    if (electronService.isElectron) {
-      console.log(process.env);
-      console.log('Run in electron');
-      console.log('Electron ipcRenderer', this.electronService.ipcRenderer);
-      console.log('NodeJS childProcess', this.electronService.childProcess);
-    } else {
-      console.log('Run in browser');
+  ngOnInit(): void {
+    this.dataSource.paginator = this.paginator;
+  }
+
+  ngAfterViewInit(): void {
+    this.dataSource.sort = this.sort;
+  }
+
+  onClickFileInputButton(): void {
+    this.fileInput.nativeElement.click();
+  }
+
+  onChangeFileInput(): void {
+    const files: { [key: string]: File } = this.fileInput.nativeElement.files;
+    this.file = files[0];
+    this.data = JSON.parse(fs.readFileSync(this.file.path));
+    this.dataSource.data = this.data;
+    this.dataSource._updateChangeSubscription();
+  }
+
+  displayedColumns: string[] = [
+    "Set",
+    "ArtifactType",
+    "Rank",
+    "Level",
+    "PrimaryStat",
+    "Substats",
+    "WeightedValue"];
+
+  applyFilter(event: Event): void {
+    const filterValue: string = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
     }
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,32 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule, HttpClient } from '@angular/common/http';
-import { CoreModule } from './core/core.module';
-import { SharedModule } from './shared/shared.module';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { HttpClientModule, HttpClient } from "@angular/common/http";
+import { CoreModule } from "./core/core.module";
+import { SharedModule } from "./shared/shared.module";
+import { FileUploadModule } from "ng2-file-upload";
+import { AppRoutingModule } from "./app-routing.module";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MatTableModule } from "@angular/material/table";
+import { MatPaginatorModule } from "@angular/material/paginator";
+import { MatIconModule } from "@angular/material/icon";
 
-import { AppRoutingModule } from './app-routing.module';
+// nG Translate
+import { TranslateModule, TranslateLoader } from "@ngx-translate/core";
+import { TranslateHttpLoader } from "@ngx-translate/http-loader";
 
-// NG Translate
-import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { HomeModule } from "./home/home.module";
+import { DetailModule } from "./detail/detail.module";
 
-import { HomeModule } from './home/home.module';
-import { DetailModule } from './detail/detail.module';
+import { AppComponent } from "./app.component";
+import { MatCommonModule, MatLineModule } from "@angular/material/core";
+import { MatSortModule } from "@angular/material/sort";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
 
-import { AppComponent } from './app.component';
-
-// AoT requires an exported function for factories
+// aoT requires an exported function for factories
 export function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
-  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+  return new TranslateHttpLoader(http, "./assets/i18n/", ".json");
 }
 
 @NgModule({
@@ -29,7 +37,17 @@ export function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
     HttpClientModule,
     CoreModule,
     SharedModule,
+    MatInputModule,
     HomeModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatCommonModule,
+    MatIconModule,
+    FileUploadModule,
+    BrowserAnimationsModule,
+    MatLineModule,
+    MatSortModule,
+    MatFormFieldModule,
     DetailModule,
     AppRoutingModule,
     TranslateModule.forRoot({
